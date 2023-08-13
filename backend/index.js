07693182145cf3d2f7754731d210d7225b16c2ff
divyasonaraa <divya.sonara@simformsolutions.com>
2023-08-13 14:13:34 +0530

    feat: get data from firebase

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,46 @@
 /* dependancies*/
 const express = require("express");
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
 
 
 /* config - express*/
 const app = express();
-const port = 3000;
+
+/*
+Config- firebase
+*/
+const serviceAccount = require("./serviceAccountKey.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: 'quasagram-82914.appspot.com'
+});
+
+const db = getFirestore();
+// const bucket = getStorage().bucket();
 
 /* endpoints*/
 app.get("/blogs", (request, response) => {
-  let blogs = [
-    {
-      id: 1,
-      title: "Example this is my first Blog",
-      content:
-        "This is mY first blog This is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blogThis is mY first blog",
-      created_at: "",
-      updated_at: "",
-    },
-  ];
-  response.send(blogs);
+  response.set("Access-Control-Allow-Origin", "*");
+  let blogs = [];
+  db.collection("blogs")
+    .orderBy("created_at", "desc")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        blogs.push(doc.data());
+         response.send(blogs);
+      });
+    })
 });
 
 
