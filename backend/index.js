293085f79f1c18cab8fd3ac6e76b1bdfc107a994
divyasonaraa <divya.sonara@simformsolutions.com>
2023-08-13 18:04:13 +0530

    feat: add edit and delete endpoints

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ let os = require("os");
 let fs = require("fs");
 let UUID = require("uuid-v4");
 
-
 /* config - express*/
 const app = express();
 
@@ -57,43 +56,81 @@ endpoint- create post
 app.post("/blog", (request, response) => {
   response.set("Access-Control-Allow-Origin", "*");
 
-  let uuid = UUID()
-  let fields = {}
-  let fileData = {}
+  let uuid = UUID();
+  let fields = {};
+  let fileData = {};
 
   const bb = busboy({ headers: request.headers });
-    bb.on('file', (name, file, info) => {
-      const { filename, encoding, mimeType } = info;
+  bb.on("file", (name, file, info) => {
+    const { filename, encoding, mimeType } = info;
 
-     let filepath =  path.join(os.tmpdir(),filename)
-     file.pipe(fs.createWriteStream(filepath))
-     fileData = {filepath , mimeType}
-    });
-    bb.on('field', (name, val, info) => {
-      console.log(`Field [${name}]: value: %j`, val);
-      fields[name] = val
-    });
-    bb.on('close', () => {
-      db.collection("blogs")
-        .doc(fields.id)
-        .set({
-          id: fields.id,
-          title: fields.title,
-          content: fields.content,
-          created_at: parseInt(fields.created_at),
-          updated_at: parseInt(fields.created_at),
-        })
-        .then(() => {
-          response.send("blog added: " + fields.id);
-        });
-    });
-    request.pipe(bb);
+    let filepath = path.join(os.tmpdir(), filename);
+    file.pipe(fs.createWriteStream(filepath));
+    fileData = { filepath, mimeType };
+  });
+  bb.on("field", (name, val, info) => {
+    console.log(`Field [${name}]: value: %j`, val);
+    fields[name] = val;
+  });
+  bb.on("close", () => {
+    db.collection("blogs")
+      .doc(fields.id)
+      .set({
+        id: fields.id,
+        title: fields.title,
+        content: fields.content,
+        created_at: parseInt(fields.created_at),
+        updated_at: parseInt(fields.created_at),
+      })
+      .then(() => {
+        response.send("blog added: " + fields.id);
+      });
+  });
+  request.pipe(bb);
 });
 
+/* endpoint - update post */
+app.put("/blog/:id", (request, response) => {
+  response.set("Access-Control-Allow-Origin", "*");
 
+  const { id } = request.params;
+  const updatedBlog = request.body; // Assuming the updated blog data is sent in the request body
+
+  db.collection("blogs")
+    .doc(id)
+    .update({
+      ...updatedBlog,
+      updated_at: FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      response.send("blog updated: " + id);
+    })
+    .catch((error) => {
+      console.error("Error updating blog:", error);
+      response.status(500).send("Internal Server Error");
+    });
+});
+
+/* endpoint - delete post */
+app.delete("/blog/:id", (request, response) => {
+  response.set("Access-Control-Allow-Origin", "*");
+
+  const { id } = request.params;
+
+  db.collection("blogs")
+    .doc(id)
+    .delete()
+    .then(() => {
+      response.send("blog deleted: " + id);
+    })
+    .catch((error) => {
+      console.error("Error deleting blog:", error);
+      response.status(500).send("Internal Server Error");
+    });
+});
 
 app.get("/", (request, response) => {
-  response.send("welcom to Blogify!");
+  response.send("welcome to Blogify!");
 });
 
 /* listen */
