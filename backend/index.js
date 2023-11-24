9e3a804e077d790733a85e2e3178a5f88b753f16
divyasonaraa <divya.sonara@simformsolutions.com>
2023-11-24 12:15:59 +0530

    fix regular base64 string

--- backend/index.js
+++ backend/index.js
@@ -40,19 +40,10 @@ const privateKey = "eN1G9mLPirxVHV3gA3usMX8xZr9Au1Qbr7VXfbNruJw";
   config - webpush
 */
 
-// Convert URL-safe Base64 to regular Base64
-const publicKeyRegularBase64 = publicKey.replace(/-/g, "+").replace(/_/g, "/");
-const privateKeyRegularBase64 = privateKey
-  .replace(/-/g, "+")
-  .replace(/_/g, "/");
-
-// // Decode the regular Base64 string
-// const decodedData = Buffer.from(regularBase64, "base64").toString("utf-8");
-
 webpush.setVapidDetails(
   "mailto:divya.sonara@simformsolutions.com",
-  publicKeyRegularBase64,
-  privateKeyRegularBase64
+  publicKey,
+  privateKey
 );
 
 /* endpoints*/
